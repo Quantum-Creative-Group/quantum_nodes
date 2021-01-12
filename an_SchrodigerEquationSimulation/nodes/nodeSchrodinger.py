5ca2d0e2394905a11b93e53954101502a0f90583
lucpng <lucpinguet91@gmail.com>
2021-01-12 15:38:08 +0100

    ✨ UI Schrodinger Node

--- an_SchrodigerEquationSimulation/nodes/nodeSchrodinger.py
+++ an_SchrodigerEquationSimulation/nodes/nodeSchrodinger.py
@@ -1,4 +1,5 @@
 import bpy
+
 from animation_nodes.base_types import AnimationNode
 
 class SchrodingerEquationSimulationNode(bpy.types.Node, AnimationNode):
@@ -6,7 +7,21 @@ class SchrodingerEquationSimulationNode(bpy.types.Node, AnimationNode):
     bl_label = "Schrödinger Equation Simulation"
 
     def create(self):
-        self.newInput("Integer", "Bonjour", "bonjour", minValue = 0, maxValue = 3)
+        self.newInput("Integer", "Frame Rate", "frame_rate", value = 25, minValue = 0)
+        self.newInput("Float", "Duration", "duration", value = 5, minValue = 0)
+        self.newInput("Float", "Δt", "Δt", value = 0.13, minValue = 0)
+
+        self.newInput("Integer", "Dimension (N)", "dimension_N", value = 10, minValue = 1)
+        self.newInput("Integer", "Size", "size", value = 5, minValue = 0)
+
+        self.newInput("Vector", "Center", "center", value = (5.,2.,3.))
+        self.newInput("Vector", "Number of waves", "number_of_waves")
+        self.newInput("Vector", "Sprawl", "sprawl")
+
+        self.newInput("Boolean", "Potential", "potential", value = 0)
+        self.newInput("Boolean", "Obstacle.s", "obstacles", value = 0)
+
+        self.newOutput("Matrix", "Output", "output")        
 
     # def execute(self, source, target, offset):
     #     if source is None or target is None:
