baa3b612692cf27f5d0148cad4607bd659b99222
Felix <felix.olart56@gmail.com>
2021-01-12 20:54:33 +0100

    Adding the algorithm files

--- an_SchrodigerEquationSimulation/nodes/nodeSchrodinger.py
+++ an_SchrodigerEquationSimulation/nodes/nodeSchrodinger.py
@@ -7,7 +7,7 @@ class SchrodingerEquationSimulationNode(bpy.types.Node, AnimationNode):
     bl_label = "Schrödinger Equation Simulation"
 
     def create(self):
-        self.newInput("Integer", "Frame Rate", "frame_rate", value = 25, minValue = 0)
+        self.newInput("Integer", "Frame rate", "frame_rate", value = 25, minValue = 0)
         self.newInput("Float", "Duration", "duration", value = 5, minValue = 0)
         self.newInput("Float", "Δt", "Δt", value = 0.13, minValue = 0)
 
@@ -21,7 +21,7 @@ class SchrodingerEquationSimulationNode(bpy.types.Node, AnimationNode):
         self.newInput("Boolean", "Potential", "potential", value = 0)
         self.newInput("Boolean", "Obstacle.s", "obstacles", value = 0)
 
-        self.newOutput("Matrix", "Output", "output")        
+        self.newOutput("Matrix", "Output", "output")
 
     # def execute(self, source, target, offset):
     #     if source is None or target is None:
