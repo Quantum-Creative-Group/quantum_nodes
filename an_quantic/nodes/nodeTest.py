6354cacc3f7e0a50f36f8fe49025e02df39d08e9
Laurine Capdeville <capdevillelaurine2@gmail.com>
2021-01-13 13:57:58 +0100

    Node with quantumcircuit socket

--- an_quantic/nodes/nodeTest.py
+++ an_quantic/nodes/nodeTest.py
@@ -1,13 +1,14 @@
 import bpy
 from animation_nodes.base_types import AnimationNode
-from an_quantic.sockets.test_socket import QuantumSocket
 
-class CopyLocationWithOffsetNode(bpy.types.Node, AnimationNode):
-    bl_idname = "an_CopyLocationWithOffsetNode"
-    bl_label = "Copy Location with Offset"
+class QuantumNode(bpy.types.Node, AnimationNode):
+    bl_idname = "an_QuantumNode"
+    bl_label = "Quantum Node"
 
     def create(self):
-        self.newInput("Quantum", "Offset", "offset")
+        self.newInput("Object", "Source", "source")
+        self.newInput("Object", "Target", "target")
+        self.newInput("QuantumCircuit", "Offset", "offset")
 
     def execute(self, source, target, offset):
         if source is None or target is None:
