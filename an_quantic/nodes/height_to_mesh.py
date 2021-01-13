4fb31623046a28e760c98a008a500cb4e0e2c75e
Elo <310.74551n@gmail.com>
2021-01-13 16:59:20 +0100

    🐛 Mesh <-> Heightmap

--- an_quantic/nodes/height_to_mesh.py
+++ an_quantic/nodes/height_to_mesh.py
@@ -1,5 +1,6 @@
 import bpy
 import numpy as np  # sqrt
+from mathutils import Vector
 from animation_nodes.base_types import AnimationNode
 
 class HeighttoMesh(bpy.types.Node, AnimationNode):
@@ -12,14 +13,16 @@ class HeighttoMesh(bpy.types.Node, AnimationNode):
         self.newInput("Vector List", "Heightmap", "heightmap")
 
     def execute(self, modify, negativeCoord, heightmap, index = 0):
-        if negativeCoord is None or heightmap is None or modify is None:
+        if negativeCoord == {} or heightmap == {} or modify is None:
             return
         """ Replaces the vertices by the new ones """
-        vertices = modify.data.vertices[:]
+        # vertices = modify.data.vertices
+        vertices = [ (modify.matrix_world @ v.co) for v in modify.data.vertices ]
         nb_vertices = len(vertices)
         n = int(np.sqrt(len(heightmap)))
         for i in range(n):
             for j in range(n):
                 if (i+1)*(j+1) < nb_vertices:
                     coord = heightmap[i,j] * negativeCoord[i,j]
-                    vertices[(i+1)*(j+1)].co[index] = coord
+                    # vertices[(i+1)*(j+1)].co[index] = coord
+                    vertices[(i+1)*(j+1)] = coord
--- an_quantic/nodes/mesh_to_height.py
+++ an_quantic/nodes/mesh_to_height.py
@@ -10,18 +10,24 @@ class MeshToHeight(bpy.types.Node, AnimationNode):
 
     def create(self):
         self.newInput("Object", "Source", "source")
-        self.newOutput("Vector List", "Negative Coordinates", "negativeCoord")  # Matrix ? Struct ? Dict ?
-        self.newOutput("Vector List", "Heightmap", "heightmap")  # Matrix ? Struct ? Dict ?
+        self.newOutput("Vector List", "Negative Coordinates", "negativeCoord")
+        self.newOutput("Vector List", "Heightmap", "heightmap")
+        self.newOutput("Vector List", "Vertices", "vertices")
 
     def execute(self, source, index = 0):
         if source is None:
-            return Vector((0, 0, 0)), Vector((0, 0, 0))
+            return Vector((0, 0, 0)), Vector((0, 0, 0)), Vector((0, 0, 0))
         """ Converts a mesh into a python dictionnary containing one of 
         the (x, y, z) coordinates. Uses the exact same logic as James Wootton 
         about QuantumBlur for images, but applied on vertices of a mesh.
         """
-        vertices = source.data.vertices[:]
-        nb_vertices = len(vertices)
+        # vertices = source.data.vertices
+        # source.data.vertices[0].co
+        vertices = [ (source.matrix_world @ v.co) for v in source.data.vertices ]
+        # verts = [vert.co for vert in bm.verts]
+        # coords = [(obj.matrix_world @ v.co) for v in obj.data.vertices]
+        # nb_vertices = len(vertices)
+        nb_vertices = 8
         # negative_coords is used to get back negative values of coordinates (if there are)
         negative_coords, height = {}, {}
         # size of the dictionnary (n*n) corresponding to the mesh
@@ -30,7 +36,7 @@ class MeshToHeight(bpy.types.Node, AnimationNode):
             for j in range(n):
                 if (i+1)*(j+1) < nb_vertices:
                     # stores absolute value of vertex coord[index]
-                    coord = vertices[(i+1)*(j+1)].co[index]
+                    coord = vertices[(i+1)*(j+1)]   # .co[index]
                     height[i,j] = np.abs(coord)
                     if coord > 0: negative_coords[i,j] = 1.0
                     else: negative_coords[i,j] = -1.0
@@ -39,4 +45,4 @@ class MeshToHeight(bpy.types.Node, AnimationNode):
                     height[i,j] = 0.
                     negative_coords[i,j] = 1.0
                     
-        return negative_coords, height
+        return negative_coords, height, vertices
