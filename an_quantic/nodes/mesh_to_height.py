f38f24dba8823a44121793cd73d8465acc77f8d4
Elo <310.74551n@gmail.com>
2021-01-13 13:19:13 +0100

    🎉 Try to convert an object into a heightmap

---
+++ an_quantic/nodes/mesh_to_height.py
@@ -0,0 +1,38 @@
+import bpy
+from animation_nodes.base_types import AnimationNode
+
+class MeshToHeight(bpy.types.Node, AnimationNode):
+    bl_idname = "an_MeshToHeight"
+    bl_label = "Mesh to Height"
+
+    def create(self):
+        self.newInput("Object", "Source", "source")
+        # self.newOutput("Object", "Heightmap", "heightmap")  # Matrix ? Struct ? Dict ?
+
+    def execute(self, source):
+        if source is None:
+            return
+        """ Converts a mesh into a python dictionnary containing one of 
+        the (x, y, z) coordinates. Uses the exact same logic as James Wootton 
+        about QuantumBlur for images, but applied on vertices of a mesh.
+        """
+        vertices = source.data.vertices[:]
+        nb_vertices = len(vertices)
+        # # negative_coords is used to get back negative values of coordinates (if there are)
+        # negative_coords, height = {}, {}
+        # # size of the dictionnary (n*n) corresponding to the mesh
+        # n = int(math.ceil(math.log(nb_vertices)/math.log(2)))
+        # for i in range(n):
+        #     for j in range(n):
+        #         if (i+1)*(j+1) < nb_vertices:
+        #             # stores absolute value of vertex coord[index]
+        #             coord = vertices[(i+1)*(j+1)].co[index]
+        #             height[i,j] = np.abs(coord)
+        #             if coord > 0: negative_coords[i,j] = 1.0
+        #             else : negative_coords[i,j] = -1.0
+        #         else:
+        #             # if the vertex n°(i+1)*(j+1) does not exist
+        #             height[i,j] = 0.
+        #             negative_coords[i,j] = 1.0
+                    
+        # return negative_coords, height
