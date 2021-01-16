6390b8f7c2a50e919017685e8891300f181c0efa
Elo <310.74551n@gmail.com>
2021-01-16 20:46:13 +0100

    🐛 ✨ Mesh <-> heightmap

--- an_quantic/nodes/quantum_heightmap/height_to_mesh.py
+++ an_quantic/nodes/quantum_heightmap/height_to_mesh.py
@@ -1,4 +1,5 @@
 import bpy
+import math # ceil
 import numpy as np  # sqrt
 from mathutils import Vector
 from animation_nodes.base_types import AnimationNode
@@ -7,22 +8,41 @@ class HeighttoMesh(bpy.types.Node, AnimationNode):
     bl_idname = "an_HeightToMesh"
     bl_label = "Height to Mesh"
 
-    def create(self):
-        self.newInput("Object", "To modify", "modify")
-        self.newInput("Vector List", "Negative Coordinates", "negativeCoord")
-        self.newInput("Vector List", "Heightmap", "heightmap")
-
-    def execute(self, modify, negativeCoord, heightmap, index = 0):
-        if negativeCoord == {} or heightmap == {} or modify is None:
-            return
-        """ Replaces the vertices by the new ones """
-        # vertices = modify.data.vertices
-        vertices = [ (modify.matrix_world @ v.co) for v in modify.data.vertices ]
+    def heights2mesh(self, vertices, negative_coords, heights):
+        """
+        [From James Wootton] Constructs an object from a set of vertices and three height dictionaries, one for each
+        direction.
+        """
         nb_vertices = len(vertices)
-        n = int(np.sqrt(len(heightmap)))
+        n = int(math.ceil(np.sqrt(nb_vertices)))
+        iterator = 0
+        h_max = [max(height.values()) for height in heights]
+
         for i in range(n):
             for j in range(n):
-                if (i+1)*(j+1) < nb_vertices:
-                    coord = heightmap[i,j] * negativeCoord[i,j]
-                    # vertices[(i+1)*(j+1)].co[index] = coord
-                    vertices[(i+1)*(j+1)] = coord
+                xyz = Vector()
+                for k, height in enumerate(heights):
+                    if (i,j) in height and iterator < nb_vertices:
+                        minus = negative_coords[k][i,j]
+                        h = float(height[i,j])
+                    else:
+                        h = 0
+                    xyz[k] = float(minus*h)
+                if iterator < nb_vertices:
+                    vertices[iterator] = xyz
+                iterator += 1
+        return vertices
+
+    def create(self):
+        self.newInput("Object", "To modify", "modify")
+        self.newInput("Vector 2D List", "Negative Coordinates", "negativeCoord")
+        self.newInput("Vector 2D List", "Heightmap", "heightmap")
+        self.newOutput("Vector List", "Vertices", "vertices")
+
+    def execute(self, modify, negativeCoord, heightmap):
+        if negativeCoord == {} or heightmap == {} or modify is None:
+            return {}
+        """ Replaces the vertices by the new ones """
+        vertices = [ (modify.matrix_world @ v.co) for v in modify.data.vertices ]
+        vertices = self.heights2mesh(vertices, negativeCoord, heightmap)
+        return vertices
