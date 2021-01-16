ba5304a801bcaba499e7accb4e3450c69fa45a93
Elo <310.74551n@gmail.com>
2021-01-16 21:50:26 +0100

    🐛 Fixed the cache problem (TODO: update .gitignore)

--- an_quantic/nodes/quantum_heightmap/height_to_circuit.py
+++ an_quantic/nodes/quantum_heightmap/height_to_circuit.py
@@ -9,11 +9,21 @@ class HeightmapToQuantumCircuitNode(bpy.types.Node, AnimationNode):
 
     def create(self):
         self.newInput("Vector List", "Heightmap", "heightmap")
-        self.newOutput("Quantum Circuit", "Quantum Circuit", "quantum_circuit")
+        self.newOutput("Quantum Circuit", "Quantum Circuit 1", "quantum_circuit_1")
+        self.newOutput("Quantum Circuit", "Quantum Circuit 2", "quantum_circuit_2")
+        self.newOutput("Quantum Circuit", "Quantum Circuit 3", "quantum_circuit_3")
 
     def execute(self, heightmap):
         try:
-            return height2circuit(heightmap)
+            # heights = _image2heights(image)
+            circuits = []
+            for height in heights:
+                circuits.append( height2circuit(height, log=log) )
+
+            qc_1 = circuits[0]
+            qc_2 = circuits[1]
+            qc_3 = circuits[2]
+            return qc_1, qc_2, qc_3
         except:
             return
         
