5d2eec6cec47add7dc57634e8207dc34ae45b01d
Laurine Capdeville <capdevillelaurine2@gmail.com>
2021-01-17 19:25:35 +0100

    Error handling for gate H

--- an_quantic/nodes/quantum_gates/gateh.py
+++ an_quantic/nodes/quantum_gates/gateh.py
@@ -6,18 +6,12 @@ from animation_nodes.base_types import AnimationNode
 class QuantumGateHNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_QuantumGateHNode"
     bl_label = "Quantum GateH"
+    errorHandlingType = "EXCEPTION"
 
     def setup(self):
         self.newInput("Quantum Circuit", "Input Circuit", "input_circuit")
         self.newInputSocket()
         self.newOutput("Quantum Circuit", "Output Circuit", "output_circuit") 
-        
-        #self.newInput("Node Control", "...")
-
-   # def create(self):
-   #     print ("create")
-   #     self.newInput("Quantum Circuit", "Input Circuit", "input_circuit")
-   #     self.newOutput("Quantum Circuit", "Output Circuit", "output_circuit")  
 
     def draw(self, layout):
         row = layout.row(align = True)
@@ -36,13 +30,6 @@ class QuantumGateHNode(bpy.types.Node, AnimationNode):
         for i, socket in enumerate(self.inputs[:]):
             if (socket.name != "Input Circuit") :
                 socketMapping[socket.identifier] = "element_"+str(i)
-        
-            #link = node.inputs[0].links[0] #gets the link from the input
-           # print() link.from_socket.name 
-        #socketMapping = {socket.identifier : "element_" + str(i) for i, socket in enumerate(self.inputs[1:])}
-
-        
-        print (socketMapping)
         return socketMapping
 
     def getExecutionCode(self, required):
@@ -50,20 +37,12 @@ class QuantumGateHNode(bpy.types.Node, AnimationNode):
             yield "try:"
             yield f"    if (element_{i} < inputCircuit.num_qubits) :"
             yield f"        inputCircuit.h(element_{i})"
+            yield f"    else :"
+            yield "        self.raiseErrorMessage(\"Qubit Index can't be larger than the number of qubits in the Input Circuit.\")"
             yield "except:"
             yield "    output_circuit = inputCircuit"
-            yield "    self.raiseErrorMessage('Failed to do Quantum operation.')"
+            yield "    self.raiseErrorMessage(\"Qubit Index can't be larger than the number of qubits in the Input Circuit.\")"
         yield "output_circuit = inputCircuit"
-        
-
-#    def edit(self):  
-#        print ("edit")
-#        emptySocket = self.inputs["..."]
-#        origin = emptySocket.directOrigin
-#        if origin is None: return
-#        socket = self.newInputSocket()
-#        socket.linkWith(origin)
-#        emptySocket.removeLinks()
 
 
     def newInputSocket(self):
