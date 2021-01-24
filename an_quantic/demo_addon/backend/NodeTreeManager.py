bf3816966c598cacd10c0d445108bf88d4ca384f
Felix <felix.olart56@gmail.com>
2021-01-24 15:45:21 +0100

    Adding : delete unsued gates

--- an_quantic/demo_addon/backend/NodeTreeManager.py
+++ an_quantic/demo_addon/backend/NodeTreeManager.py
@@ -12,6 +12,9 @@ class NodeTreeManager:
         self.gf = GatesFactory(self.demo_id)
     
     def generateNodeTree(self, obj):
+        """
+        Generates the demo node trees
+        """
         genereateMultiplyAll(bpy.ops.node, self.demo_id)
         generateMaxValue(bpy.ops.node, self.demo_id)
         generateNegative(bpy.ops.node, self.demo_id)
@@ -22,10 +25,16 @@ class NodeTreeManager:
         self.node_tree = bpy.data.node_groups[self.main_tree_id + "an_q"]
     
     def updateTarget(self, obj):
+        """
+        Updates the target of the quantum circuits
+        """
         self.node_tree.nodes[self.main_tree_id + "mesh_obj_input" + "_main"].inputs[0].object = obj
         self.node_tree.nodes[self.main_tree_id + "obj_instancer" + "_main"].inputs[1].object = obj
     
     def update(self, new_circuits):
+        """
+        Updates the circuit node trees
+        """
         modif = self.getModification(self.last_circuits, new_circuits)
         if(modif != (None, None, None)):
             circuit_node_tree = bpy.data.node_groups[self.demo_id + "circuit_" + modif[2][0]]
@@ -47,16 +56,50 @@ class NodeTreeManager:
                     current_gate.newInputSocket()
                     # forces to update the tree (magic trick)
                     bpy.context.scene.frame_set(bpy.data.scenes['Scene'].frame_current)
-                    print(len(current_gate.inputs) - 1, modif[2][1])
                     current_gate.inputs[len(current_gate.inputs) - 2].value = modif[2][1]
             elif(modif[0] == "DEL"):
-                print("DEL")
-            print(modif)
+                # delete a gate
+                if(self.gateIsStillUsed(modif[1], new_circuits[modif[2][0]])):
+                    # the gate is still used
+                    print("GATE " + modif[1].upper() + " : still used")
+                else:
+                    # the gate isn't used anymore
+                    current_gate = circuit_node_tree.nodes[self.demo_id + "gate_" + modif[1] + circuit_id]
+                    # save the input and output
+                    inp = current_gate.originNodes[0].outputs[0]
+                    out = current_gate.outputs[0].directTargets[0]
+                    self.removeLink(current_gate.originNodes[0].outputs[0], current_gate.inputs[len(current_gate.inputs) - 1], circuit_node_tree)
+                    self.removeLink(current_gate.outputs[0], current_gate.outputs[0].directTargets[0], circuit_node_tree)
+                    current_gate.remove()
+                    # link the saved output and input
+                    circuit_node_tree.links.new(inp, out)
+        
+        # sets last ciruits to the current circuits
         self.last_circuits = copy.deepcopy(new_circuits)
+
+    def addGate(self, new_gate, circuit_tree, q_index):
+        """
+        Adds a new gate in the given circuit node tree
+        """
+        circuit_tree.nodes.new(type="an_QuantumGate" + new_gate.type + "Node")
+        gate_node = circuit_tree.nodes["Quantum Gate " + new_gate.type]
+        gate_node.name = new_gate.name
+        gate_node.location = new_gate.location
+        gate_node.inputs[0].value = q_index
+        circuit_tree.links.new(new_gate.input.outputs[0], gate_node.inputs[1])
+        circuit_tree.links.new(gate_node.outputs[0], new_gate.output.inputs[0])
+        return
+    
+    def removeGate(self):
+        return
     
     @classmethod
     def getModification(cls, last_circuits, new_circuits):
-        # returns : action, gate_type, (circuit name, qubit index, gate index)
+        """
+        Returns the informations about the modification done on the circuits
+
+        Returns : action, gate_type, (circuit name, qubit index, gate index)
+        """
         for circ_name in ["x", "y", "z"]:
             q_index = 0
             for qubit in new_circuits[circ_name].data:
@@ -81,22 +124,23 @@ class NodeTreeManager:
             return True
         except:
             return False
-
-    def addGate(self, new_gate, circuit_tree, q_index):
-        circuit_tree.nodes.new(type="an_QuantumGate" + new_gate.type + "Node")
-        gate_node = circuit_tree.nodes["Quantum Gate " + new_gate.type]
-        gate_node.name = new_gate.name
-        gate_node.location = new_gate.location
-        gate_node.inputs[0].value = q_index
-        circuit_tree.links.new(new_gate.input.outputs[0], gate_node.inputs[1])
-        circuit_tree.links.new(gate_node.outputs[0], new_gate.output.inputs[0])
-        return
     
     @classmethod
     def removeLink(cls, inp, out, node_tree):
+        """
+        Removes the link between the given input and output
+        """
         for link in node_tree.links:
             if(link.from_socket == inp and link.to_socket == out):
                 node_tree.links.remove(link)
     
-    def removeGate(self):
-        return
+    @classmethod
+    def gateIsStillUsed(cls, gate_type, circuit):
+        """
+        Returns True if the given gate type is still used in the circuit
+        """
+        for qubit in circuit.data:
+            for gate in qubit:
+                if gate == gate_type:
+                    return True
+        return False
