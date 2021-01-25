291131681355f7cf54597732e3f0e831b56c1a03
Felix <felix.olart56@gmail.com>
2021-01-25 17:41:05 +0100

    Fixing bug : delete target

--- an_quantic/demo_addon/operators/DeleteTarget.py
+++ an_quantic/demo_addon/operators/DeleteTarget.py
@@ -5,15 +5,11 @@ class DeleteTarget(Operator):
     bl_idname = "object.delete_target"
     bl_label = "Delete Target"
     bl_description = "Deletes the current target"
-
-    @classmethod
-    def poll(cls, context):
-        if(context.object == None): return False
-        return context.object.select_get() and bpy.context.active_object == bpy.types.Scene.demo_manager.target
     
     def execute(self, context):
         self.report({'INFO'}, "AN_Q_DEMO : target successfully removed")
-        bpy.types.Scene.demo_manager.removeAll()
+        if(bpy.types.Scene.demo_manager.target != None):
+            bpy.types.Scene.demo_manager.removeAll()
         return{'FINISHED'}
 
     def invoke(self, context, event):
