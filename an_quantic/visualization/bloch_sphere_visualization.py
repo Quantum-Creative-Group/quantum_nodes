8ad429d75128bc869dd49471faf47c4f94bb7b33
Laurine Capdeville <capdevillelaurine2@gmail.com>
2021-01-26 14:47:35 +0100

    Dessin sphere

--- an_quantic/visualization/bloch_sphere_visualization.py
+++ an_quantic/visualization/bloch_sphere_visualization.py
@@ -5,82 +5,8 @@ import math
 import numpy as np
 from qiskit import *
 from qiskit.visualization.utils import _bloch_multivector_data
-
-def get_bloch_sphere_angles(qubit_state):
-    #input = two complexe numbers alpha and beta wich represent the state of the qubit
-    #alpha = a +i b, beta = c + i d
-    _alpha = qubit_state[0]
-    _beta = qubit_state[1]
-    a = _alpha.real
-    b = _alpha.imag
-    c = _beta.real
-    d = _beta.imag
     
-    if(a==0 and b == 0 and c == 0 and d == 0):
-        return
-    if (b==0):
-        theta = 2*np.arccos(a)
-        if(c == 0 and d == 0):
-            phi = 0
-        elif(c == 0):
-            if(d>0):
-                phi = np.pi/2
-            else:
-                phi = -np.pi/2
-        else:
-            if (c > 0):
-                phi = np.arctan(d/c)
-            else:
-                phi = np.arctan(d/c) + np.pi
-    else: 
-        theta = 2*np.arccos(abs(_alpha)) 
-        if (a==0):
-            if(b > 0):
-                global_phase = np.pi/2
-            else:
-                global_phase = -np.pi/2
-        else:
-            if(a> 0):
-                global_phase = np.arctan (b/a)
-            else:
-                global_phase = np.arctan (b/a) + np.pi
-
-        B = (d*np.cos(global_phase)-c*np.sin(global_phase))
-        A = (c*np.cos(global_phase)+d*np.sin(global_phase))
-        if (A==0):
-            if(B==0):
-                phi = np.pi/2
-            else:
-                phi = -np.pi/2
-        else:
-            if(A>0):
-                phi = np.arctan(B/A)
-            else:
-                phi = np.arctan(B/A) + np.pi
-    return theta, phi 
-
-def get_angles(x, y, z):
-    r = math.sqrt(x**2 + y**2 + z**2)
-    if (r<10**-15):
-        theta = 0
-        phi = 0
-    else :
-        theta = np.arccos(z/r)
-        if (x==0):
-            if(y > 0):
-                phi = np.pi/2
-            else:
-                phi = -np.pi/2
-        else:
-            if(x> 0):
-                phi = np.arctan (y/x)
-            else:
-                phi = np.arctan (y/x) + np.pi
-    return r, theta, phi
-        
-    
-
-def bloch_sphere(state, qubit_index):
+def bloch_sphere():
     bloch_data = _bloch_multivector_data(state)
     _theta = get_angles(bloch_data[qubit_index][0],bloch_data[qubit_index][1],bloch_data[qubit_index][2])[1]
     _phi = get_angles(bloch_data[qubit_index][0],bloch_data[qubit_index][1],bloch_data[qubit_index][2])[2]
@@ -195,8 +121,6 @@ def bloch_sphere(state, qubit_index):
     bmesh.ops.translate(bm, verts = bm.verts, vec = mathutils.Vector((0.0, 0.0, 1.)))
     bmesh.ops.create_cone(bm, cap_ends=False, cap_tris=False, segments=20, diameter1=0.05, diameter2=0.05, depth=1.9)
     bmesh.ops.translate(bm, verts = bm.verts, vec = mathutils.Vector((0.0, 0.0, 0.95)))
-    bmesh.ops.rotate(bm, verts = bm.verts, cent =  (0.0, 0.0, 0.0), matrix=mathutils.Matrix.Rotation(_theta, 3, 'Y'))
-    bmesh.ops.rotate(bm, verts = bm.verts, cent =  (0.0, 0.0, 0.0), matrix=mathutils.Matrix.Rotation(_phi, 3, 'Z'))
     bm.to_mesh(mesh_vector)
     bm.free()
     #--------------------------------------------------------------------
@@ -254,27 +178,8 @@ def bloch_sphere(state, qubit_index):
     font_objy.select_set(True)
     
     #Join all objects
+    vector.select_set(False)
     bpy.context.view_layer.objects.active = sphere 
     bpy.ops.object.join()
+    vector.select_set(True)
     bpy.ops.object.parent_set()
-    
-def bloch_spheres_circuit(quantum_circuit):
-    backend = Aer.get_backend('statevector_simulator') # Tell Qiskit how to simulate our circuit
-    result = execute(quantum_circuit,backend).result() # Do the simulation, returning the result
-    out_state = result.get_statevector()
-    for i in range (quantum_circuit.num_qubits):
-        bloch_sphere(out_state, i)
-    
-
-quantumtest1 = QuantumCircuit(5) 
-quantumtest1.rx(3,0)
-quantumtest1.rx(12,1)
-quantumtest1.rx(12,2)
-quantumtest1.h(1)
-quantumtest1.x(1)
-quantumtest1.ry(1.67,0)
-backend1 = Aer.get_backend('statevector_simulator') # Tell Qiskit how to simulate our circuit
-result1 = execute(quantumtest1,backend1).result() # Do the simulation, returning the result
-out_state1 = result1.get_statevector()
-
-bloch_spheres_circuit(quantumtest1)
