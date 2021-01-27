93721b697959c1d6eee1d92f90b7fba17958ccbd
Elo <310.74551n@gmail.com>
2021-01-27 14:01:38 +0100

    🎨 Clean code in Schrödinger

--- an_quantic/algorithms/schrodinger_equation/SimulationCache.py
+++ an_quantic/algorithms/schrodinger_equation/SimulationCache.py
@@ -16,8 +16,8 @@ class SimulationCache:
         @parameter :
         max_frame - integer - the length of the simulation, in frames
         """
-        self._data = np.empty((max_frame), np.ndarray)
-        self._last_computed_frame = None
+        self.data = np.empty((max_frame), np.ndarray)
+        self.last_computed_frame = None
     
     def __processFrame(self, d, inp):
         """
@@ -28,19 +28,19 @@ class SimulationCache:
         d   - SimulationDataManager
         inp - SimulationInputsManager
         """
-        vector_selon_x = d.xConcatenate(d._wave_function, inp._dimension)
-        vector_derive_y_selon_x = d.xConcatenate(d.dySquare(d._wave_function, inp._dimension, inp._step), inp._dimension)
+        vector_selon_x = d.xConcatenate(d.wave_function, inp.dimension)
+        vector_derive_y_selon_x = d.xConcatenate(d.dySquare(d.wave_function, inp.dimension, inp.step), inp.dimension)
         U_selon_x = vector_selon_x + (1j*inp._delta_t/2)*(vector_derive_y_selon_x - d._v_x*vector_selon_x)
         U_selon_x_plus = scipy.sparse.linalg.spsolve(d._hx, U_selon_x)
 
-        d._wave_function = d.xDeconcatenate(U_selon_x_plus, inp._dimension)
+        d.wave_function = d.xDeconcatenate(U_selon_x_plus, inp.dimension)
 
-        vector_selon_y = d.yConcatenate(d._wave_function, inp._dimension)
-        vector_derive_x_selon_y = d.yConcatenate(d.dxSquare(d._wave_function, inp._dimension, inp._step), inp._dimension)
-        U_selon_y = vector_selon_y  + (1j*inp._delta_t/2)*(vector_derive_x_selon_y - d._v_y*vector_selon_y)
-        U_selon_y_plus = scipy.sparse.linalg.spsolve(d._hy, U_selon_y)
+        vector_selon_y = d.yConcatenate(d.wave_function, inp.dimension)
+        vector_derive_x_selon_y = d.yConcatenate(d.dxSquare(d.wave_function, inp.dimension, inp.step), inp.dimension)
+        U_selon_y = vector_selon_y  + (1j*inp._delta_t/2)*(vector_derive_x_selon_y - d.v_y*vector_selon_y)
+        U_selon_y_plus = scipy.sparse.linalg.spsolve(d.hy, U_selon_y)
 
-        d._wave_function = d.yDeconcatenate(U_selon_y_plus, inp._dimension)
+        d.wave_function = d.yDeconcatenate(U_selon_y_plus, inp.dimension)
     
 
     def getFrame(self, frame, d, inp):
@@ -54,27 +54,27 @@ class SimulationCache:
         d       - SimulationDataManager
         inp     - SimulationInputsManager
         """
-        if(frame >= 0):
-            if(frame <= self._last_computed_frame):
+        if (frame >= 0):
+            if (frame <= self.last_computed_frame):
                 # the frame has already been computed
-                return self._data[frame]
-            elif(frame <= inp._frame_rate * inp._duration):
+                return self.data[frame]
+            elif (frame <= inp.frame_rate * inp.duration):
                 # the frame is in the scope of the simulation
                 # but has not been computed yet
-                frames_to_compute = frame - self._last_computed_frame
+                frames_to_compute = frame - self.last_computed_frame
                 try:
                     for frame in range(frames_to_compute):
                         self.__processFrame(d, inp)
-                        self._last_computed_frame += 1
-                        self._data[self._last_computed_frame] = d._wave_function
+                        self.last_computed_frame += 1
+                        self.data[self.last_computed_frame] = d.wave_function
                 except Exception as e:
                     print(sys.exc_info())
                     raise e from e
-                return self._data[frame]
+                return self.data[frame]
             else:
                 # the requested frame is not in the scope of the simulation
                 # return the last computed frame by default
-                return self._data[self._last_computed_frame]
+                return self.data[self.last_computed_frame]
         else:
             # the requested frame is negative
             raise IndexError("ERROR::SimulationCache::getFrame() frame index out of range\nData for frame n°" + str(frame) + " unavailable")
