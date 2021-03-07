f8ac6af2c21b92acc47c786a91f5654ab269e358
Laurine Capdeville <capdevillelaurine2@gmail.com>
2021-03-07 22:47:45 +0100

    🧹 CLEAN: plotEmptyHistogram

--- quantum_nodes/visualization/empty_graphs/graphs_utils.py
+++ quantum_nodes/visualization/empty_graphs/graphs_utils.py
@@ -24,3 +24,18 @@ def creatMesh(name, r, g, b, t):
     bpy.context.view_layer.objects.active = object
     object.select_set(True)
     return mesh, object
+
+def creatMultipleText(name, position_x, position_y, 
+              rotation_x, rotation_y, rotation_z,size) :
+    curves_left = {}
+    text_objects_left = {}
+    for i in range (11):
+        curves_left["font_curve_{0}".format(i)] = bpy.data.curves.new(type="FONT",name=name +str(i))
+        curves_left["font_curve_{0}".format(i)].body = str(i/10)
+        text_objects_left["font_obj_{0}".format(i)] = bpy.data.objects.new(name+str(i), curves_left["font_curve_{0}".format(i)])
+        text_objects_left["font_obj_{0}".format(i)].rotation_euler = (rotation_x, rotation_y, rotation_z)
+        text_objects_left["font_obj_{0}".format(i)].scale = (0.2,0.2,0.2)
+        text_objects_left["font_obj_{0}".format(i)].location = (position_x,position_y,(size*i)/10 - 0.07)
+        bpy.context.collection.objects.link(text_objects_left["font_obj_{0}".format(i)])
+        bpy.context.view_layer.objects.active = text_objects_left["font_obj_{0}".format(i)]
+        text_objects_left["font_obj_{0}".format(i)].select_set(True)
