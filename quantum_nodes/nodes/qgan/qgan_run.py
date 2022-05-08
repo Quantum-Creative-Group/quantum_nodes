a13ded475343e3e63b6cd528009ce11608833ae5
Zelkins10 <mdoyen4@gmail.com>
2022-05-08 18:42:09 +0200

    Update licenses

--- quantum_nodes/nodes/qgan/qgan_run.py
+++ quantum_nodes/nodes/qgan/qgan_run.py
@@ -1,3 +1,15 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020s.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 import bpy
 from animation_nodes.base_types import AnimationNode
 
@@ -118,5 +130,5 @@ class RunQGAN(bpy.types.Node, AnimationNode):
         return num_epochs, qgan
 
         # ------------- TRAINING PROGRESS AND OUTCOME --------------
-
+        
         # [moved to separate files]
